52e816ff51df24d88cdedb3d61dfc0e323de8bbd
Chad Robinson <chad_robinson@apple.com>
2019-05-11 18:27:18 -0700

    iana registry module and tests

---
+++ iana/iana.go
@@ -0,0 +1,188 @@
+/*
+Package iana imports the Internet Assigned Numbers Authority (IANA) IP Special
+Registries as a data structure and implements functions to compare the reserved
+networks against iplib.Net objects. The IANA registry is used to reserve
+portions of IP network space for special use, examples being the IPv4 Private
+Use blocks (10.0.0/8, 172.16.0.0/12 and 192.168.0.0/16) and the IPv6 netblock
+set aside for documentation purposes (2001:db8::/32).
+
+Note that this package does not contain historical reservations. So IPv6
+2001:10::/28 (ORCHIDv1) is listed in the document as "deprecated" and not
+present in this library.
+
+The data-set for the IANA registries is available from:
+- https://www.iana.org/assignments/iana-ipv4-special-registry/iana-ipv4-special-registry.xhtml
+- https://www.iana.org/assignments/iana-ipv6-special-registry/iana-ipv6-special-registry.xhtml
+*/
+package iana
+
+import (
+	"github.com/c-robinson/iplib"
+	"net"
+	"sort"
+)
+
+// Reservation describes an entry in the IANA IP Special Registry
+type Reservation struct {
+
+	// Network is the reserved network
+    Network     iplib.Net
+
+    // Title is a name given to the reservation
+	Title       string
+
+    // RFC is the list of relevant RFCs
+	RFC         []string
+
+    // true if a router may forward packets bound for this network between
+    // external interfaces
+	Forwardable bool
+
+    // true if a router may pass packets bound for this network outside of
+    // a private network
+	Global      bool
+
+    // true if an IP implementation must implement this policy in order to
+    // be compliant
+	Reserved    bool
+}
+
+// Registry holds the aggregated network list from IANA's v4 and v6 registries.
+// Only the following fields were imported: Address Block, Name, RFC,
+// Forwardable, Globally Reachable and Reserved-by-Protocol
+var Registry = []*Reservation{
+	&Reservation { getFromCIDR("0.0.0.0/8"),          "This host on this network",                 []string{ "RFC1122" },            false, false, true },
+	&Reservation { getFromCIDR("10.0.0.0/8"),         "Private-Use",                               []string{ "RFC1918" },            true,  false,false },
+	&Reservation { getFromCIDR("100.64.0.0/10"),      "Shared Address Space",                      []string{ "RFC6598" },            false, false, true },
+	&Reservation { getFromCIDR("127.0.0.0/8"),        "Loopback",                                  []string{ "RFC1122" },            false, false, true },
+	&Reservation { getFromCIDR("169.254.0.0/16"),     "Link Local",                                []string{ "RFC3927" },            false, false, true },
+	&Reservation { getFromCIDR("172.16.0.0/12"),      "Private-Use",                               []string{ "RFC1918" },            true,  false, false },
+	&Reservation { getFromCIDR("192.0.0.0/24"),       "IETF Protocol Assignments",                 []string{ "RFC6890" },            false, false, false },
+	&Reservation { getFromCIDR("192.0.0.0/29"),       "IPv4 Service Continuity Prefix",            []string{ "RFC7335" },            true,  false, false },
+	&Reservation { getFromCIDR("192.0.0.8/32"),       "IPv4 dummy address",                        []string{ "RFC7600" },            false, false, false },
+	&Reservation { getFromCIDR("192.0.0.9/32"),       "Port Control Protocol Anycast",             []string{ "RFC7723" },            true,  true,  true },
+	&Reservation { getFromCIDR("192.0.0.10/32"),      "Traversal Using Relays around NAT Anycast", []string{ "RFC8155" },            true,  true,  false },
+	&Reservation { getFromCIDR("192.0.0.170/32"),     "NAT64/DNS64 Discovery",                     []string{ "RFC7050" },            false, false, true },
+	&Reservation { getFromCIDR("192.0.0.171/32"),     "NAT64/DNS64 Discovery",                     []string{ "RFC7050" },            false, false, true },
+	&Reservation { getFromCIDR("192.0.2.0/24"),       "Documentation (TEST-NET-1)",                []string{ "RFC5737" },            false, false, false },
+	&Reservation { getFromCIDR("192.31.196.0/24"),    "AS112-v4",                                  []string{ "RFC7535" },            true,  true,  false },
+	&Reservation { getFromCIDR("192.52.193.0/24"),    "AMT",                                       []string{ "RFC7450" },            true,  true,  false },
+	&Reservation { getFromCIDR("192.168.0.0/16"),     "Private-Use",                               []string{ "RFC1918" },            true,  false, false },
+	&Reservation { getFromCIDR("192.175.48.0/24"),    "Direct Delegation AS112 Service",           []string{ "RFC7534" },            true,  true,  false },
+	&Reservation { getFromCIDR("198.18.0.0/15"),      "Benchmarking",                              []string{ "RFC2544" },            true,  false, false },
+	&Reservation { getFromCIDR("198.51.100.0/24"),    "Documentation (TEST-NET-2)",                []string{ "RFC5737" },            false, false, false },
+	&Reservation { getFromCIDR("203.0.113.0/24"),     "Documentation (TEST-NET-3)",                []string{ "RFC5737" },            false, false, false },
+	&Reservation { getFromCIDR("240.0.0.0/4"),        "Reserved",                                  []string{ "RFC1112" },            false, false, true },
+	&Reservation { getFromCIDR("255.255.255.255/32"), "Limited Broadcast",                         []string{ "RFC8190", "RFC919" },  false, false, true },
+	&Reservation { getFromCIDR("::1/128"),            "Loopback Address",                          []string{ "RFC4291" },            false, false, true },
+	&Reservation { getFromCIDR("::/128"),             "Unspecified Address",                       []string{ "RFC4291" },            false, false, true },
+	&Reservation { getFromCIDR("::ffff:0:0/96"),      "IPv4-mapped Address",                       []string{ "RFC4291" },            false, false, true },
+	&Reservation { getFromCIDR("64:ff9b::/96"),       "IPv4-IPv6 Translation",                     []string{ "RFC6052" },            true,  true,  false },
+	&Reservation { getFromCIDR("64:ff9b:1::/48"),     "IPv4-IPv6 Translation",                     []string{ "RFC8215" },            true,  false, false },
+	&Reservation { getFromCIDR("100::/64"),           "Discard-Only Address Block",                []string{ "RFC6666" },            true,  false, false },
+	&Reservation { getFromCIDR("2001::/23"),          "IETF Protocol Assignments",                 []string{ "RFC2928" },            false, false, false },
+	&Reservation { getFromCIDR("2001::/32"),          "TEREDO",                                    []string{ "RFC4380", "RFC8190" }, true,  true,  false },
+	&Reservation { getFromCIDR("2001:1::1/128"),      "Port Control Protocol Anycast",             []string{ "RFC7723" },            true,  true,  false },
+	&Reservation { getFromCIDR("2001:1::2/128"),      "Traversal Using Relays around NAT Anycast", []string{ "RFC8155" },            true,  true,  false },
+	&Reservation { getFromCIDR("2001:2::/48"),        "Benchmarking",                              []string{ "RFC5180", "RFC1752" }, true,  false, false },
+	&Reservation { getFromCIDR("2001:3::/32"),        "AMT",                                       []string{ "RFC7450" },            true,  true,  false },
+	&Reservation { getFromCIDR("2001:4:112::/48"),    "AS112-v6",                                  []string{ "RFC7535" },            true,  true,  false },
+	&Reservation { getFromCIDR("2001:5::/32"),        "EID Space for LISP (Managed by RIPE NCC)",  []string{ "RFC7954" },            true,  true,  true },
+	&Reservation { getFromCIDR("2001:20::/28"),       "ORCHIDv2",                                  []string{ "RFC7343" },            true,  true,  false },
+	&Reservation { getFromCIDR("2001:db8::/32"),      "Documentation",                             []string{ "RFC3849" },            false, false, false },
+	&Reservation { getFromCIDR("2002::/16"),          "6to4",                                      []string{ "RFC3056" },            true,  true,  false },
+	&Reservation { getFromCIDR("2620:4f:8000::/48"),  "Direct Delegation AS112 Service",           []string{ "RFC7534" },            true,  true,  false },
+	&Reservation { getFromCIDR("fc00::/7"),           "Unique-Local",                              []string{ "RFC4193", "RFC8190" }, true,  false, false },
+	&Reservation { getFromCIDR("fe80::/10"),          "Link-Local Unicast",                        []string{ "RFC4291" },            false, false, true },
+}
+
+// GetReservationsForNetwork returns a list of any IANA reserved networks
+// that are either part of the supplied network or that the supplied network
+// is part of
+func GetReservationsForNetwork(n iplib.Net) []*Reservation {
+	reservations := []*Reservation{}
+	for _, r := range Registry {
+		if r.Network.ContainsNet(n) || n.ContainsNet(r.Network) {
+			reservations = append(reservations, r)
+		}
+	}
+	return reservations
+}
+
+// GetReservationsForIP returns a list of any IANA reserved networks that
+// the supplied IP is part of
+func GetReservationsForIP(ip net.IP) []*Reservation {
+	reservations := []*Reservation{}
+	for _, r := range Registry {
+		if r.Network.Contains(ip) {
+			if iplib.EffectiveVersion(ip) == 4 && r.Title == "IPv4-mapped Address" {
+				continue
+			}
+			reservations = append(reservations, r)
+		}
+	}
+	return reservations
+}
+
+// GetRFCsForNetwork returns a list of all RFCs that apply to the given
+// network
+func GetRFCsForNetwork(n iplib.Net) []string {
+	rfclist := []string{}
+	reservations := GetReservationsForNetwork(n)
+	if len(reservations) > 0 {
+		for _, r := range reservations {
+			LOOP: for _, rfc := range r.RFC {
+				for _, xrfc := range rfclist {
+					if xrfc == rfc {
+						continue LOOP
+					}
+				}
+				rfclist  = append(rfclist, rfc)
+			}
+		}
+		sort.Strings(rfclist)
+	}
+	return rfclist
+}
+
+// IsForwardable will return false if the given iplib.Net contains or is
+// contained in a network that is marked not-forwardable in the IANA registry
+func IsForwardable(n iplib.Net) bool {
+	reservations := GetReservationsForNetwork(n)
+	for _, r := range reservations {
+		if r.Forwardable == false {
+			return false
+		}
+	}
+	return true
+}
+
+// IsGlobal will return false if the given iplib.Net contains or is contained
+// in a network that is marked not-global in the IANA registry
+func IsGlobal(n iplib.Net) bool {
+	reservations := GetReservationsForNetwork(n)
+	for _, r := range reservations {
+		if r.Global == false {
+			return false
+		}
+	}
+	return true
+}
+
+// IsReserved  will return true if the given iplib.Net contains or is
+// contained in a network that is marked reserved-by-protocol in the IANA
+// registry
+func IsReserved(n iplib.Net) bool {
+	reservations := GetReservationsForNetwork(n)
+	for _, r := range reservations {
+		if r.Reserved == true {
+			return true
+		}
+	}
+	return false
+}
+
+func getFromCIDR(s string) iplib.Net {
+	_, n, _ := iplib.ParseCIDR(s)
+	return n
+}
---
+++ iana/iana_test.go
@@ -0,0 +1,213 @@
+package iana
+
+import (
+	"github.com/c-robinson/iplib"
+	"net"
+	"testing"
+)
+
+var IPTests = []struct {
+	name		string
+	address     string
+	resCount	int
+}{
+	{
+		"NotReservedv4",
+		"144.21.1.19",
+		0,
+	},
+	{
+		"Reservedv4",
+		"192.168.123.49",
+		1,
+	},
+	{
+		"NotReservedv6",
+		"25:100:200::195:16",
+		0,
+	},
+	{
+		"Reservedv6",
+		"2001:db8:1::250:3",
+		1,
+	},
+}
+
+func TestGetReservationsForIP(t *testing.T) {
+	for _, tt := range IPTests {
+		ip := net.ParseIP(tt.address)
+		r := GetReservationsForIP(ip)
+		if len(r) != tt.resCount {
+			t.Errorf("'%s' expected %d reservations, got %d", tt.name, tt.resCount, len(r))
+			for _, v := range r {
+				t.Logf("%s, %s", v.Title, v.Network.String())
+			}
+		}
+	}
+}
+
+var NetTests = []struct {
+	name            string
+	resCount        int
+	network         string
+	rfcList         []string
+	valForwardable  bool
+	valGlobal       bool
+	valReserved     bool
+}{
+	{
+		"NotReservedv4",
+		0,
+		"1.0.0.0/8",
+		[]string{},
+		true,
+		true,
+		false,
+	},
+	{
+		"Reservedv4",
+		1,
+		"10.0.0.0/8",
+		[]string{"RFC1918"},
+		true,
+		false,
+		false,
+	},
+	{
+		"ContainedInReservedv4",
+		1,
+		"10.44.1.0/24",
+		[]string{"RFC1918"},
+		true,
+		false,
+		false,
+	},
+	{
+		"ContainsReservedv4",
+		1,
+		"192.168.0.0/16",
+		[]string{"RFC1918"},
+		true,
+		false,
+		false,
+	},
+	{
+		"MultipleReservationsv4",
+		8,
+		"192.0.0.0/12",
+		[]string{"RFC5737", "RFC6890", "RFC7050", "RFC7335", "RFC7600", "RFC7723", "RFC8155"},
+		false,
+		false,
+		true,
+	},
+
+	{
+		"NotReservedv6",
+		0,
+		"2001:db7::/32",
+		[]string{},
+		true,
+		true,
+		false,
+	},
+	{
+		"Reservedv6",
+		1,
+		"2001:db8::/32",
+		[]string{"RFC3849"},
+		false,
+		false,
+		false,
+	},
+	{
+		"ContainedInReservedv6",
+		1,
+		"2001:db8:100::/40",
+		[]string{"RFC3849"},
+		false,
+		false,
+		false,
+	},
+	{
+		"ContainsReservedv6",
+		1,
+		"2001:d0::/28",
+		[]string{"RFC2928"},
+		false,
+		false,
+		false,
+	},
+	{
+		"MultipleReservationsv6",
+		10,
+		"2001::/16",
+		[]string{"RFC1752", "RFC2928", "RFC3849", "RFC4380", "RFC5180", "RFC7343", "RFC7450", "RFC7535", "RFC7723", "RFC7954", "RFC8155", "RFC8190"},
+		false,
+		false,
+		true,
+	},
+}
+
+func TestGetReservationsForNetwork(t *testing.T) {
+	for _, tt := range NetTests {
+		_, n, _ := iplib.ParseCIDR(tt.network)
+		r := GetReservationsForNetwork(n)
+		if len(r) != tt.resCount {
+			t.Errorf("'%s' expected %d reservations, got %d", tt.name, tt.resCount, len(r))
+			for _, v := range r {
+				t.Logf("%s, %s", v.Title, v.Network.String())
+			}
+		}
+	}
+}
+
+func TestGetRFCsForNetwork(t *testing.T) {
+	for _, tt := range NetTests {
+		_, n, _ := iplib.ParseCIDR(tt.network)
+		rfclist := GetRFCsForNetwork(n)
+		if v := equalList(rfclist, tt.rfcList); v != true {
+			t.Errorf("'%s' (%s) expected %v, got %v", tt.name, tt.network, tt.rfcList, rfclist )
+		}
+	}
+}
+
+func TestIsForwardable(t *testing.T) {
+	for _, tt := range NetTests {
+		_, n, _ := iplib.ParseCIDR(tt.network)
+		if tt.valForwardable != IsForwardable(n) {
+			t.Errorf("'%s' (%s) expected %t, got %t", tt.name, tt.network, tt.valForwardable, IsForwardable(n))
+		}
+	}
+}
+
+func TestIsGlobal(t *testing.T) {
+	for _, tt := range NetTests {
+		_, n, _ := iplib.ParseCIDR(tt.network)
+		if tt.valGlobal != IsGlobal(n) {
+			t.Errorf("'%s' (%s) expected %t, got %t", tt.name, tt.network, tt.valGlobal, IsGlobal(n))
+		}
+	}
+}
+
+func TestIsReserved(t *testing.T) {
+	for _, tt := range NetTests {
+		_, n, _ := iplib.ParseCIDR(tt.network)
+		if tt.valReserved != IsReserved(n) {
+			t.Errorf("'%s' (%s) expected %t, got %t", tt.name, tt.network, tt.valReserved, IsReserved(n))
+		}
+	}
+}
+
+func equalList(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
